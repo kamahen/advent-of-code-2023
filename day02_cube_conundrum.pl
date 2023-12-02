c317c5734bdb262c28b15d7f1205f978c818d680
Peter Ludemann <peter.ludemann@gmail.com>
2023-12-02 15:03:31 -0800

    Added possible_grame

--- day02_cube_conundrum.pl
+++ day02_cube_conundrum.pl
@@ -67,6 +67,17 @@ is the sum of the IDs of those games?
 
 solve(_) :- fail.
 
+possible_game(Game, Grabs) :-
+    maplist(possible_grab(Game), Grabs).
+
+possible_grab(Game, Grab) :-
+    dict_pairs(Grab, _, GrabPairs),
+    maplist(possible_grab_pair(Game), GrabPairs).
+
+possible_grab_pair(Game, Color-Number) :-
+    get_dict(Color, Game, GameNumber),
+    GameNumber >= Number.
+
 parse_game(Line, GameNumber, Grabs) :-
     string_codes(Line, Codes),
     phrase(parse_game(GameNumber, Grabs), Codes).
@@ -99,22 +110,28 @@ color(blue) --> "blue".
 test(parse) :-
     parse_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", G, V),
     assertion(G == 1),
-    assertion(V == [grab{blue:3, red:4}, grab{red:1, green:2, blue:6}, grab{green:2}]).
+    assertion(V == [grab{blue:3, red:4}, grab{red:1, green:2, blue:6}, grab{green:2}]),
+    assertion(possible_game(game{red:12, green:13, blue:14}, V)).
 test(parse) :-
     parse_game("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", G, V),
     assertion(G == 2),
-    assertion(V == [grab{blue:1, green:2}, grab{green:3, blue:4, red:1}, grab{green:1, blue:1}]).
+    assertion(V == [grab{blue:1, green:2}, grab{green:3, blue:4, red:1}, grab{green:1, blue:1}]),
+    assertion(possible_game(game{red:12, green:13, blue:14}, V)).
 test(parse) :-
     parse_game("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", G, V),
     assertion(G == 3),
-    assertion(V == [grab{green:8, blue:6, red:20}, grab{blue:5, red:4, green:13}, grab{green:5, red:1}]).
+    assertion(V == [grab{green:8, blue:6, red:20}, grab{blue:5, red:4, green:13}, grab{green:5, red:1}]),
+    assertion(\+ possible_game(game{red:12, green:13, blue:14}, V)).
 test(parse) :-
     parse_game("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", G, V),
     assertion(G == 4),
-    assertion(V == [grab{green:1, red:3, blue:6}, grab{green:3, red:6}, grab{green:3, blue:15, red:14}]).
+    assertion(V == [grab{green:1, red:3, blue:6}, grab{green:3, red:6}, grab{green:3, blue:15, red:14}]),
+    assertion(\+ possible_game(game{red:12, green:13, blue:14}, V)).
 test(parse) :-
     parse_game("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", G, V),
     assertion(G == 5),
-    assertion(V == [grab{red:6, blue:1, green:3}, grab{blue:2, red:1, green:2}]).
+    assertion(V == [grab{red:6, blue:1, green:3}, grab{blue:2, red:1, green:2}]),
+    assertion(possible_game(game{red:12, green:13, blue:14}, V)).
+
 
 :- end_tests(day2).
