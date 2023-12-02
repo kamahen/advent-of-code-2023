5d03266c002732276205cb924a5c140e6da412c1
Peter Ludemann <peter.ludemann@gmail.com>
2023-12-01 20:22:54 -0800

    Added "Part Two" text, reformatted

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 See also https://adventofcode.com/2023
 
+All of these were done with [SWI-Prolog](https://www.swi-prolog.org/) unless otherwise noted.
+
 ## Day 1
 
 Quite straightforward.
--- day01_trebuchet.pl
+++ day01_trebuchet.pl
@@ -1,18 +1,40 @@
 % -*- mode: Prolog -*-
 
 /*
+https://adventofcode.com/2023/day/1
+
 --- Day 1: Trebuchet?! ---
-Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
-You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+Something is wrong with global snow production, and you've been
+selected to take a look. The Elves have even given you a map; on it,
+they've used stars to mark the top fifty locations that are likely to
+be having problems.
 
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+You've been doing this long enough to know that to restore snow
+operations, you need to check all fifty stars by December 25th.
 
-You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+Collect stars by solving puzzles. Two puzzles will be made available
+on each day in the Advent calendar; the second puzzle is unlocked when
+you complete the first. Each puzzle grants one star. Good luck!
 
-As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+You try to ask why they can't just use a weather machine ("not
+powerful enough") and where they're even sending you ("the sky") and
+why your map looks mostly blank ("you sure ask a lot of questions")
+and hang on did you just say the sky ("of course, where do you think
+snow comes from") when you realize that the Elves are already loading
+you into a trebuchet ("please hold still, we need to strap you in").
 
-The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+As they're making the final adjustments, they discover that their
+calibration document (your puzzle input) has been amended by a very
+young Elf who was apparently just excited to show off her art
+skills. Consequently, the Elves are having trouble reading the values
+on the document.
+
+The newly-improved calibration document consists of lines of text;
+each line originally contained a specific calibration value that the
+Elves now need to recover. On each line, the calibration value can be
+found by combining the first digit and the last digit (in that order)
+to form a single two-digit number.
 
 For example:
 
@@ -20,9 +42,38 @@ For example:
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-Consider your entire calibration document. What is the sum of all of the calibration values?
+In this example, the calibration values of these four lines are 12,
+38, 15, and 77. Adding these together produces 142.
+
+Consider your entire calibration document. What is the sum of all of
+the calibration values?
+
+Your puzzle answer was 56049.
+
+--- Part Two ---
+
+Your calculation isn't quite right. It looks like some of the digits
+are actually spelled out with letters: one, two, three, four, five,
+six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real
+first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14,
+and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+
+Your puzzle answer was 54530.
 */
 
 :- module(day01, [run_tests/0, solve/1]).
@@ -55,7 +106,6 @@ first_digit(Dir, First) -->
 digit(fwd, 0) --> "zero".
 digit(fwd, 1) --> "one".
 digit(fwd, 2) --> "two".
-digit(fwd, 3) --> "three".
 digit(fwd, 4) --> "four".
 digit(fwd, 5) --> "five".
 digit(fwd, 6) --> "six".
